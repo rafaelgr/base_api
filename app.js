e786dfa038160325d8bdfa46aa2e6506e714edcb
Rafael Garcia <rafa@myariadna.com>
2018-10-25 18:00:08 +0200

    Use dotenv

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const Express = require('express');
+const Cors = require('cors');
+const Dotenv = require('dotenv');
+
+Dotenv.config();
+
 const app = Express();
+app.use(Cors());
 
 app.get('/', (req, res) => {
     res.status(200).send(' / Get');
 });
 
-const server = app.listen(30000, () => {
+const server = app.listen(process.env.BASE_PORT, () => {
     console.log("Listening on port " + server.address().port + "...");
 });
 
