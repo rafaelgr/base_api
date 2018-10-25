54082bfecf49bb520e79e7c1197b30db2b21bb67
Rafael Garcia <rafa@myariadna.com>
2018-10-25 18:54:41 +0200

    Loggers

---
+++ api/version/version_controller.js
@@ -0,0 +1,15 @@
+const Express = require('express');
+const Pack = require('../../package.json');
+
+var router = Express.Router();
+
+router.get('/', (req, res) => {
+    var version = {
+        name: Pack.name,
+        version: Pack.version,
+        description: Pack.description
+    }
+    res.json(version);
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const Express = require('express');
 const Cors = require('cors');
 const Dotenv = require('dotenv');
+const Pack = require('./package.json');
+const Morgan = require('morgan');
+const Winston = require('./winston');
 
 Dotenv.config();
 
 const app = Express();
+
+app.use(Morgan('combined', { stream: Winston.stream }));
 app.use(Cors());
 
-app.get('/', (req, res) => {
-    res.status(200).send(' / Get');
-});
+app.use('/version', require('./api/version/version_controller'));
 
 const server = app.listen(process.env.BASE_PORT, () => {
+    console.log(Pack.name + " VRS: " + Pack.version);
     console.log("Listening on port " + server.address().port + "...");
 });
 
