7c4bc1121bf3a5491381f5d10a2f506b775d49f0
Rafael Garcia <rafa@myariadna.com>
2018-10-30 12:06:15 +0100

    Jasmine as a local dependency

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,5 +1,23 @@
 "use strict";
 const Request = require('request');
+const Dotenv = require('dotenv');
+const Pack = require('../package.json')
+
+describe("Configuration", () => {
+    beforeAll(() => {
+        Dotenv.config();
+    });
+    it("BASE_PORT", () => {
+        expect(process.env.BASE_PORT).not.toBeUndefined();
+    });
+    it("BASE_WINSTON_FILELEVEL", () => {
+        expect(process.env.BASE_WINSTON_FILELEVEL).not.toBeUndefined();
+    });
+    it("BASE_WINSTON_CONSOLELEVEL", () => {
+        expect(process.env.BASE_WINSTON_CONSOLELEVEL).not.toBeUndefined();
+    });
+});
+
 
 describe("Server", () => {
     var server;
